64b552c3b7acdb6efba12fa32b16f94cc65a220b
Andrea Monterrossa <moralesandrea.edr4@gmail.com>
2022-12-08 10:26:06 -0600

    refactor consts city and country

--- facade/main.py
+++ facade/main.py
@@ -1,7 +1,8 @@
 from classes.Facade import Facade
 
 WEATHER_API_KEY = 'YOUR OWN OPEN WEATHER API KEY'
-
+CITY='San Salvador'
+COUNTRY='SV'
 if __name__ == '__main__':
     facade = Facade()
-    print facade.get_forecast('London', 'UK', WEATHER_API_KEY)
+    print facade.get_forecast(CITY,COUNTRY, WEATHER_API_KEY)
